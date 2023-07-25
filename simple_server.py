906d234af61e43f85f8d0c202c355066448b8793
Ruthuwamahoro <r.uwamahoro@alustudent.com>
2023-07-25 23:13:37 +0200

    finally

--- simple_server.py
+++ simple_server.py
@@ -1,9 +1,13 @@
 #creating my simple HTTP server
 import http.server
-class RequestHandler(http.server.SimpleHTTPRequestHandler):
+class CGIRequestHandler(http.server.CGIHTTPRequestHandler):
     def do_GET(self):
-        return http.server.SimpleHTTPRequestHandler.do_GET(self)
-handler = RequestHandler
+        if self.path =='/':
+            self.path = 'index.html'
+        elif self.path == '/page':
+            self.path = '/cgi-bin/page.py'
+        return http.server.CGIHTTPRequestHandler.do_GET(self)
+handler = CGIRequestHandler
 PORT = 5000 #specifies the port on which the server will listen for incoming HTTP requests.
 try:
     with http.server.HTTPServer(("0.0.0.0", PORT), handler) as server:
